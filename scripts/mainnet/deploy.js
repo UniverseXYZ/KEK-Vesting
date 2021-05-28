4ecc06ffda26157e082c34991159e2f40cbb562e
bc <bara.catalin@gmail.com>
2021-05-28 18:06:07 +0300

    estimate gas price for each transaction

--- scripts/mainnet/deploy.js
+++ scripts/mainnet/deploy.js
@@ -1,4 +1,5 @@
-const { ethers } = require('@nomiclabs/buidler')
+const { ethers } = require('@nomiclabs/buidler');
+const { default: axios } = require('axios');
 
 const Erc20abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"burn","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"burnFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"mint","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}];
 
@@ -425,18 +426,32 @@ async function main () {
         },
         ];
 
+    let lastGasEstimateTimestamp = 0;
+    let crtTimestamp;
+
+    let gasPrice = 0;
+
     let currentVesting
     let startIndex = 0
     let amount
     for (let i = startIndex; i < owners.length; i++) {
+
+        crtTimestamp = Math.floor(new Date().getTime() / 1000)
+        if (crtTimestamp - lastGasEstimateTimestamp > 30) {
+            const price = await axios.get('https://www.gasnow.org/api/v3/gas/price?utm_source=UniverseXYZVestingDeployer');
+            gasPrice = Math.round(1.15 * price.data.data.fast);
+            console.log(`got new price at ${crtTimestamp}, value: ${gasPrice}`);
+            lastGasEstimateTimestamp = crtTimestamp;
+        }
+
         const owner = owners[i];
         amount = ethers.BigNumber.from(owner.amount)
             .mul(ethers.BigNumber.from(10).pow(18))
 
-        currentVesting = await Vesting.deploy(owner.address, _xyzTokenAddress, startTime, amount, {gasPrice: 20_000_000_000})
+        currentVesting = await Vesting.deploy(owner.address, _xyzTokenAddress, startTime, amount, {gasPrice: gasPrice})
         await currentVesting.deployed()
 
-        await XYZToken.transfer(currentVesting.address, amount)
+        await XYZToken.transfer(currentVesting.address, amount, {gasPrice: gasPrice})
 
         console.log(`Deploy vesting contract index. ${i} for ${owner.address} at ${currentVesting.address}, amount ${owner.amount}`)
     }
