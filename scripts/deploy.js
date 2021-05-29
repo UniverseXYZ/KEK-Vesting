82d87e526e05d6b81f7b8e55962f5236fe956cb8
bc <bara.catalin@gmail.com>
2021-05-29 12:49:37 +0300

    test rinkeby

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,108 +1,62 @@
-const { ethers } = require('@nomiclabs/buidler')
+const { ethers } = require('@nomiclabs/buidler');
+const { default: axios } = require('axios');
 
-async function main () {
+const Erc20abi = [{ "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "spender", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "previousOwner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "newOwner", "type": "address" }], "name": "OwnershipTransferred", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "Transfer", "type": "event" }, { "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }, { "internalType": "address", "name": "spender", "type": "address" }], "name": "allowance", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "approve", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "account", "type": "address" }], "name": "balanceOf", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "burn", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "account", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "burnFrom", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "decimals", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "subtractedValue", "type": "uint256" }], "name": "decreaseAllowance", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "addedValue", "type": "uint256" }], "name": "increaseAllowance", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "mint", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "name", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "owner", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "renounceOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "symbol", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "totalSupply", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "recipient", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "transfer", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "sender", "type": "address" }, { "internalType": "address", "name": "recipient", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "transferFrom", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "newOwner", "type": "address" }], "name": "transferOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }];
+
+async function main() {
     const Vesting = await ethers.getContractFactory('Vesting')
-    const VestingRouter = await ethers.getContractFactory('VestingRouter')
 
-    const bondTokenAddress = '0x0391D2021f89DC339F60Fff84546EA23E337750f'
-    const startTime = 1603065600
+    const _xyzTokenAddress = '0x41e88dc0dfa5455e64327484f1862332413520da'
+    const XYZToken = await ethers.getContractAt(Erc20abi, _xyzTokenAddress);
 
-    const owners = {
-        fourRC: {
-            address: '0xfc775166c7ba5DFDA475FAAB4f03D8006adBe763',
-            amount: 225000,
+    const startTime = 1622419200 - 15*3600*24
+
+    const owners = [
+        {
+            "address": "0x92f58192353a2d759D21cA85854d2B92A7762a72",
+            "amount": 100
         },
-        parafi: {
-            address: '0x8Db0401F6De4adDBE50EDe7b1460852ae837dBDd',
-            amount: 187500,
-        },
-        kain: {
-            address: '0x3d4AD2333629eE478E4f522d60A56Ae1Db5D3Cdb',
-            amount: 75000,
-        },
-        centrality: {
-            address: '0x660C1750CEa5893a20F762b1E32353c1c825Ff1F',
-            amount: 75000,
-        },
-        proofSystems: {
-            address: '0xFdc45E8A348Afbf8F59d9611BF6Cc634B99E2aE4',
-            amount: 37500,
-        },
-        kinHodl: {
-            address: '0x52EebD82826a7B530839424cEAb6CA3643eB0Ef1',
-            amount: 37500,
-        },
-        aaron: {
-            address: '0x3483573534982883edef09dedd57c0c90825fbee',
-            amount: 37500,
-        },
-        blockchainCompanies: {
-            address: '0x51c60c3cd234430A238f33559D8d4aA646741820',
-            amount: 18750,
-        },
-        andrew: {
-            address: '0x5d76A92b7cB9E1A81B8eb8c16468F1155B2f64f4',
-            amount: 18750,
-        },
-        dahret: {
-            address: '0x569ad69df4aD0ca6E466dd670A72475De94309D3',
-            amount: 18750,
-        },
-        stani: {
-            address: '0xF5Fb27b912D987B5b6e02A1B1BE0C1F0740E2c6f',
-            amount: 18750,
-        },
-        dmob: {
-            address: '0x01772953ed3B69349088aE7824c649d6dcd0cB1E',
-            amount: 650000,
-        },
-        tyler: {
-            address: '0x747dfb7D6D27671B4e3E98087f00e6B023d0AAb7',
-            amount: 300000,
-        },
-        rudeLabs: {
-            address: '0x61C47FaC14afBe2e03DdbFef3695026328E83a83',
-            amount: 300000,
-        },
-        aaronAdvisor: {
-            address: '0x3483573534982883edef09dedd57c0c90825fbee',
-            amount: 100000,
-        },
-        atpar: {
-            address: '0xb1dC62436Bc918FAfbF84E413028FB6CA185aDBa',
-            amount: 100000,
-        },
-    }
+        ];
+
+    let lastGasEstimateTimestamp = 0;
+    let crtTimestamp;
+
+    let gasPrice = 0;
 
     let currentVesting
-    const vestingArray = []
-    const amountArray = []
-    const amountArrayString = []
-    let counter = 1
+    let startIndex = 0
     let amount
-    for (const key in owners) {
-        amount = ethers.BigNumber.from(owners[key].amount)
-            .mul(ethers.BigNumber.from(10).pow(18))
+    for (let i = startIndex; i < owners.length; i++) {
 
-        currentVesting = await Vesting.deploy(owners[key].address, bondTokenAddress, startTime, amount)
-        await currentVesting.deployed()
+        crtTimestamp = Math.floor(new Date().getTime() / 1000)
+        if (crtTimestamp - lastGasEstimateTimestamp > 120) {
+            try{
+                const price = await axios.get('https://www.gasnow.org/api/v3/gas/price?utm_source=UniverseXYZVestingDeployer');
+                gasPrice = Math.round(1.15 * price.data.data.fast);
+                console.log(`got new price at ${crtTimestamp}, value: ${gasPrice}`);
+            } catch (e) {
+                console.log('price req timed out');
+            }
+            lastGasEstimateTimestamp = crtTimestamp;
+        }
 
-        vestingArray.push(currentVesting.address)
-        amountArray.push(ethers.BigNumber.from(amount))
-        amountArrayString.push(amount.toString())
+        if (gasPrice < 250_000_000_000) {
+            const owner = owners[i];
+            amount = ethers.BigNumber.from(owner.amount)
+                .mul(ethers.BigNumber.from(10).pow(18))
 
-        console.log(`Vesting #${counter} deployed at address ${currentVesting.address} with amount: ${amount.toString()}`)
+            console.log(`Start deploy vesting contract index ${i}`);
+            currentVesting = await Vesting.deploy(owner.address, _xyzTokenAddress, startTime, amount, { gasPrice: gasPrice })
+            await currentVesting.deployed()
 
-        counter++
+            await XYZToken.transfer(currentVesting.address, amount, { gasPrice: gasPrice })
+
+            console.log(`Deployed vesting contract index ${i} for ${owner.address} at ${currentVesting.address}, amount ${owner.amount.toLocaleString()}`)
+        } else {
+            console.log('Too expensive to deploy');
+            break;
+        }
     }
-
-    console.log(vestingArray)
-    console.log(amountArrayString)
-
-    const vestingRouter = await VestingRouter.deploy(vestingArray, amountArray, bondTokenAddress)
-    await vestingRouter.deployed()
-
-    console.log(`Vesting Router deployed at address ${vestingRouter.address} with BOND ${bondTokenAddress}`)
 }
 
 main()
